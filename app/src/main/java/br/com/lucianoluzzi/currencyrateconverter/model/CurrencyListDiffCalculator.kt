3fe82248a1fb9ea9adcd6b237a7d6ab72d3e0038
Luciano <lucianoluzzi@hotmail.com>
2019-06-09 16:11:20 -0300

    refactoring

---
+++ app/src/main/java/br/com/lucianoluzzi/currencyrateconverter/model/CurrencyListDiffCalculator.kt
@@ -0,0 +1,33 @@
+package br.com.lucianoluzzi.currencyrateconverter.model
+
+import android.os.Bundle
+import androidx.recyclerview.widget.DiffUtil
+
+class CurrencyListDiffCalculator(private val oldList: List<Currency>, private val newList: List<Currency>) {
+    fun calculateDiff(): DiffUtil.DiffResult {
+        val diff = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return oldList[oldItemPosition].id == newList[newItemPosition].id
+            }
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return oldList[oldItemPosition] == newList[newItemPosition]
+            }
+
+            override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+                val diffBundle = Bundle()
+                val oldItem = oldList[oldItemPosition]
+                val newItem = newList[newItemPosition]
+                if (oldItem.value == newItem.value)
+                    diffBundle.putDouble(Currency.CURRENCY_VALUE_KEY, newItem.value.toDouble())
+
+                return diffBundle
+            }
+
+            override fun getOldListSize() = oldList.size
+
+            override fun getNewListSize() = newList.size
+        }, true)
+        return diff
+    }
+}
--- app/src/main/java/br/com/lucianoluzzi/currencyrateconverter/view/binding_adapters/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/br/com/lucianoluzzi/currencyrateconverter/view/binding_adapters/RecyclerViewBindingAdapter.kt
@@ -1,11 +1,10 @@
 package br.com.lucianoluzzi.currencyrateconverter.view.binding_adapters
 
-import android.os.Bundle
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.lucianoluzzi.currencyrateconverter.model.Currency
+import br.com.lucianoluzzi.currencyrateconverter.model.CurrencyListDiffCalculator
 import br.com.lucianoluzzi.currencyrateconverter.view.CurrencyAdapter
 
 @BindingAdapter("rates_adapter", requireAll = false)
@@ -23,31 +22,9 @@ fun RecyclerView.setCurrencies(currencies: List<Currency>?) {
     val oldList = currencyAdapter.currencies
     val newList = currencies.orEmpty()
 
-    val diff = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldList[oldItemPosition].id == newList[newItemPosition].id
-        }
-
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-            return oldList[oldItemPosition] == newList[newItemPosition]
-        }
-
-        override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
-            val diffBundle = Bundle()
-            val oldItem = oldList[oldItemPosition]
-            val newItem = newList[newItemPosition]
-            if (oldItem.value == newItem.value)
-                diffBundle.putDouble(Currency.CURRENCY_VALUE_KEY, newItem.value.toDouble())
-
-            return diffBundle
-        }
-
-        override fun getOldListSize() = oldList.size
-
-        override fun getNewListSize() = newList.size
-    }, true)
-
+    val diff = CurrencyListDiffCalculator(oldList, newList).calculateDiff()
     currencyAdapter.currencies.clear()
     currencyAdapter.currencies.addAll(newList)
+
     diff.dispatchUpdatesTo(currencyAdapter)
 }
